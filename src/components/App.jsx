aee13fe57ed8cf5a39d0a2eddedefd3c0e361704
TarasKovalyshyn <taras.kovalyshyn@gmail.com>
2022-11-29 23:51:50 +0200

    application release
    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
+import { Profile } from './userTask/User';
+import user from '../data/user.json';
+
+import { Transactions } from './transactionsTask/transactions';
+import transactions from '../data/transactions.json';
+
+import statistics from 'data/statistics.json';
+import { Statistics } from './statisticsTask/statistics';
+
+import friends from 'data/friends.json';
+import { Friends } from './friendsTask/friends';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Transactions items={transactions} />
+      <Statistics items={statistics} />
+      <Friends items={friends} />
+
+      
+    </>
   );
-};
+}
