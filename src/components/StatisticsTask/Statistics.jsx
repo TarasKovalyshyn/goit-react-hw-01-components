f757fefecd3222af44cc6cd3d426469814c095e7
TarasKovalyshyn <taras.kovalyshyn@gmail.com>
2022-12-01 15:38:36 +0200

    comit

--- src/components/StatisticsTask/Statistics.jsx
+++ src/components/StatisticsTask/Statistics.jsx
@@ -9,7 +9,7 @@ export const Statistics = ({ title, stats }) => {
     <section className={css.statistics}>
       {title && <h2 className={css.title}>Upload stats</h2>}
 
-      <ul className={css.statList}>
+      <ul className={css['statList']}>
         {stats.map(({ id, label, percentage }) => (
           <li
             className={css.item}
